64a03f733ddbcbb5577acf10a8db1a9ad815be71
EC2 Default User <ec2-user@ip-172-31-43-230.ap-northeast-1.compute.internal>
2023-04-25 03:45:42 +0000

    [ADD]日記編集削除実装

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -2,25 +2,41 @@ class DiariesController < ApplicationController
   def new
     @diary = Diary.new
   end
-  
+
   def create
     @diary = Diary.new(diary_params)
     @diary.user_id = current_user.id
-    @diary.save
-    redirect_to post_images_path
+    if @diary.save
+      redirect_to request.referer
+    else
+      render :new
+    end
   end
 
   def index
+    @diary = Diary.all
   end
 
   def show
+    @diary = Diary.find(params[:id])
+  end
+
+  def edit
+    @diary = Diary.find(params[:id])
+  end
+
+  def update
+    @diary = Diary.find(params[:id])
+    @diary.update(diary_params)
+    redirect_to diary_path(diaries.id)
   end
 
   def destroy
+    @diary = Diary.find(params[:id])
+    @diary.destroy
+    redirect_to request.referer
   end
 
-  
-  # 投稿データのストロングパラメータ
   private
 
   def diary_params
